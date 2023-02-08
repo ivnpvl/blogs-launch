91b3e629ccb64ada00ff217c7b5c201ca29149c8
Ivan Pavlov <divaythfyr@mail.ru>
2023-02-08 23:45:35 +0300

    Add templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Стартовая страница Yatube.')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
