8451aea9b1ef9003b44bf67abfe88a462fb63182
Ivan Pavlov <divaythfyr@mail.ru>
2023-02-10 13:40:13 +0300

    Add url names

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Стартовая страница группы {slug}.')
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
