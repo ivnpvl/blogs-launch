49ef256382b10564fb9fed5146420ba4ded73dff
Ivan Pavlov <divaythfyr@mail.ru>
2023-02-14 23:14:10 +0300

    Add datetime and lang

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,8 +5,8 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.CharField(max_length=200)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
     description = models.TextField()
 
     def __str__(self):
